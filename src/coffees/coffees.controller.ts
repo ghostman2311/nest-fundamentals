480a91257596f8d836f765c8b9dc2fc34c1735ef
Nikhil <nkarnwal07@gmail.com>
2022-04-12 21:27:29 +0530

    Response status code

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Res } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
     @Get()
-    findAll(){
-    return 'This is coffee endpoint';
+    findAll(@Res() response){
+     //response.status(200).send('This is coffee endpoint');
+     return 'This is Coffee endpoint'
     }
 
     @Get(':id')
@@ -13,6 +14,7 @@ export class CoffeesController {
     }
 
     @Post()
+    //@HttpCode(HttpStatus.GONE)
     create(@Body('Name') body){
         return body;
     }
